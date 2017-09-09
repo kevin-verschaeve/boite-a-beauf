87330c89863ffe5275f58aeb018f0187b67d69bd
Kevin Verschaeve <kevin.verschaeve@live.fr>
2017-09-09 20:01:18 +0200

    do not play same file to early

--- src/Player.php
+++ src/Player.php
@@ -11,11 +11,13 @@ class Player
     const RECORDS_DIR = '/var/www/html/sounds/records';
 
     const COMMAND = 'omxplayer -o local';
+    const PLAYED_FILE = 'data/played.txt';
 
     public function playRandom()
     {
         $file = $this->findRandomFile();
         $this->playFile($file);
+        $this->addToPlayed($file);
     }
 
     public function playFile($file)
@@ -29,11 +31,29 @@ class Player
         $finder = new Finder();
         $finder->files()->in([self::BASIC_DIR, self::RECORDS_DIR]);
 
+        $played = file(self::PLAYED_FILE);
+
         $files = [];
         foreach ($finder as $file) {
+            if (in_array($file, $played, true)) {
+                continue;
+            }
+
             $files[] = $file->getPathname();
         }
 
         return $files[array_rand($files)];
+    }
+
+    private function addToPlayed($file)
+    {
+        $played = file(self::PLAYED_FILE);
+
+        if (count($played) >= 50) {
+            unset($played[0]);
+        }
+        $played[] = $file;
+
+        file_put_contents(self::PLAYED_FILE, $played);
     }
 }
