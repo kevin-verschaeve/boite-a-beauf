24e4087fb19dd8c7f8a59ffd052202b2919a5132
Kevin Verschaeve <kevin.verschaeve@live.fr>
2017-09-09 20:01:18 +0200

    do not play same file to early

--- src/Player.php
+++ src/Player.php
@@ -7,8 +7,11 @@ use Symfony\Component\Process\Process;
 
 class Player
 {
-    const BASIC_DIR = '/var/www/html/sounds/basics';
-    const RECORDS_DIR = '/var/www/html/sounds/records';
+    const ROOT_PATH = '/var/www/html';
+
+    const BASIC_DIR = self::ROOT_PATH.'/sounds/basics';
+    const RECORDS_DIR = self::ROOT_PATH.'/sounds/records';
+    const PLAYED_FILE = self::ROOT_PATH.'/data/played.txt';
 
     const COMMAND = 'omxplayer -o local';
 
@@ -16,6 +19,7 @@ class Player
     {
         $file = $this->findRandomFile();
         $this->playFile($file);
+        $this->addToPlayed($file);
     }
 
     public function playFile($file)
@@ -29,11 +33,29 @@ class Player
         $finder = new Finder();
         $finder->files()->in([self::BASIC_DIR, self::RECORDS_DIR]);
 
+        $played = file(self::PLAYED_FILE);
+
         $files = [];
         foreach ($finder as $file) {
+            if (in_array($file, $played, true)) {
+                continue;
+            }
+
             $files[] = $file->getPathname();
         }
 
         return $files[array_rand($files)];
+    }
+
+    private function addToPlayed($file)
+    {
+        $played = file(self::PLAYED_FILE);
+
+        if (count($played) >= 50) {
+            unset($played[0]);
+        }
+        $played[] = $file;
+
+        file_put_contents(self::PLAYED_FILE, implode("\n", $played));
     }
 }
