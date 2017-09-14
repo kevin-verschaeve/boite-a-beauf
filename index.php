09b40ec0fbc7e19f3f0d0b7027e1e7bc2ed27cee
Kevin Verschaeve <kevin.verschaeve@live.fr>
2017-09-14 14:38:14 +0200

    Design website

--- index.php
+++ index.php
@@ -12,19 +12,22 @@ sort($files);
 
 <html>
 <head>
+    <link rel="stylesheet" href="assets/lib/basscss.min.css">
     <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
-    <h1>La boite a beauf</h1>
-    <ul>
-        <?php foreach ($files as $file) : ?>
-        <li>
-            <a href="#" class="js-play" data-sound="<?php echo Utils::getPublicPath($file); ?>">
-                <?php echo Utils::humanizePath($file); ?>
-            </a>
-        </li>
-        <?php endforeach; ?>
-    </ul>
+    <div class="mx-auto max-width-4">
+        <h1>La boite a beauf</h1>
+        <div class="flex flex-wrap">
+            <?php foreach ($files as $file) : ?>
+                <div class="sm-col-6 md-col-4 border-box wrap">
+                    <a href="#" class="js-play bold text-decoration-none center block" data-sound="<?php echo Utils::getPublicPath($file); ?>">
+                        <?php echo Utils::humanizePath($file); ?>
+                    </a>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 
     <script src="assets/lib/jquery-3.2.1.slim.min.js"></script>
     <script src="assets/lib/soundjs-0.6.2.min.js"></script>
