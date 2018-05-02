3586119c9f90133fada2ac59edad3eb96acaf31d
Kevin Verschaeve <kevin.verschaeve@live.fr>
2018-05-02 21:46:45 +0200

    Allow certain ips

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -11,16 +11,18 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class PlayerController
 {
     private $secret;
+    private $authorizedIps;
 
-    public function __construct(string $secret)
+    public function __construct(string $secret, array $authorizedIps)
     {
         $this->secret = $secret;
+        $this->authorizedIps = $authorizedIps;
     }
 
     public function __invoke(Request $request, Player $player)
     {
-        if (false === $request->isXmlHttpRequest() && $this->secret !== $request->request->get('secret')) {
-            throw new NotFoundHttpException();
+        if (!(\in_array($request->getClientIp(), $this->authorizedIps, true) || $this->secret === $request->request->get('secret'))) {
+            throw new NotFoundHttpException('This page does not exists.');
         }
 
         $sound = $request->request->get('sound');
