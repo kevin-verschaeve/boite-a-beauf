0ccb1bb957ce11629f28e358cb8c7728543fbcef
Kevin Verschaeve <kevin.verschaeve@live.fr>
2018-12-16 01:02:22 +0100

    Upgrade SF to 4.2

--- config/bundles.php
+++ config/bundles.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
-    'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle' => ['all' => true],
-    'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
-    'Symfony\Bundle\DebugBundle\DebugBundle' => ['dev' => true, 'test' => true],
-    'Symfony\Bundle\WebProfilerBundle\WebProfilerBundle' => ['dev' => true, 'test' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
